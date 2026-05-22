e133171895995710fb0ee456e93dd090199fd4d9
Adam Jattiem <adamjattiem12@gmail.com>
2026-05-22 10:16:28 +0200

    Add files via upload

---
+++ Intro To JavaScript Exercise 3/arrays.js
@@ -0,0 +1,8 @@
+let colors = [];
+colors.push("red", "blue", "purple");
+let numbers = [1, 2, 3, 4, 5];
+let removed = colors.pop();
+colors.unshift("black");
+console.log(colors.length);
+console.log(numbers.length);
+
